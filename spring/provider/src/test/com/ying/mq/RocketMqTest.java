fe8505bc4f71fa17855ce72016a25c8368a153ab
leo <zhaoleoboy@163.com>
2019-07-27 22:57:06 +0800

    添加rocketMQ，集成有点复杂，明天尝试kafka

---
+++ spring/provider/src/test/com/ying/mq/RocketMqTest.java
@@ -0,0 +1,46 @@
+package com.ying.mq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.junit.Test;
+import org.springframework.util.StopWatch;
+
+public class RocketMqTest {
+
+    @Test
+    public void testProduce(){
+        String producerGroup = "Producer";
+        String namesrvAddr = "192.168.31.205:9876";
+        //生产者的组名
+        DefaultMQProducer producer = new DefaultMQProducer(producerGroup);
+        //指定NameServer地址，多个地址以 ; 隔开
+        producer.setNamesrvAddr(namesrvAddr);
+        try {
+            /**
+             * Producer对象在使用之前必须要调用start初始化，初始化一次即可
+             * 注意：切记不可以在每次发送消息时，都调用start方法
+             */
+            producer.start();
+
+            //创建一个消息实例，包含 topic、tag 和 消息体
+            //如下：topic 为 "TopicTest"，tag 为 "push"
+            Message message = new Message("TopicTest", "push", "发送消息----zhisheng-----".getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+            StopWatch stop = new StopWatch();
+            stop.start();
+
+            for (int i = 0; i < 100; i++) {
+                SendResult result = producer.send(message);
+                System.out.println("发送响应：MsgId:" + result.getMsgId() + "，发送状态:" + result.getSendStatus());
+            }
+            stop.stop();
+            System.out.println("----------------发送一万条消息耗时：" + stop.getTotalTimeMillis());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.shutdown();
+        }
+    }
+}
