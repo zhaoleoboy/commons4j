fd46d90045c705bea422a0d28b3213ced540bc30
zhaoleo <zhaoleoboy@163.com>
2019-07-29 14:26:54 +0800

    base

---
+++ spring/order/src/test/java/com/ying/controller/OkHttpTest.java
@@ -0,0 +1,36 @@
+package com.ying.controller;
+
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class OkHttpTest {
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    /**
+     * 测试get方法
+     * @throws IOException
+     */
+    @Test
+    public void testGet() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://publicobject.com/helloworld.txt")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+        Headers responseHeaders = response.headers();
+        for (int i = 0; i < responseHeaders.size(); i++) {
+            System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+        }
+
+        System.out.println(response.body().string());
+
+    }
+}
