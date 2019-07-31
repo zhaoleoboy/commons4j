e0fe9be3c518949f3eb392c25d0689246a2c55b2
zhaoleo <zhaoleoboy@163.com>
2019-07-31 15:56:47 +0800

    netty

---
+++ netty/src/main/java/com/ying/NettyServerApp.java
@@ -0,0 +1,18 @@
+package com.ying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class NettyServerApp {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(NettyServerApp.class);
+    }
+}
---
+++ netty/src/main/java/com/ying/config/NettyProperties.java
@@ -0,0 +1,93 @@
+package com.ying.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@ConfigurationProperties(ignoreUnknownFields = true, prefix = "server")
+public class NettyProperties {
+
+    private Boolean enabled;
+    private String bindAddress;
+    private int bindPort;
+    private Netty netty;
+
+    public Netty getNetty() {
+        return netty;
+    }
+
+    public void setNetty(Netty netty) {
+        this.netty = netty;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getBindAddress() {
+        return bindAddress;
+    }
+
+    public void setBindAddress(String bindAddress) {
+        this.bindAddress = bindAddress;
+    }
+
+    public int getBindPort() {
+        return bindPort;
+    }
+
+    public void setBindPort(int bindPort) {
+        this.bindPort = bindPort;
+    }
+
+    public static class Netty {
+
+        private String leakDetectorLevel;
+        private int bossGroupThreadCount;
+        private int workerGroupThreadCount;
+        private String maxPayloadSize;
+
+        public int getBossGroupThreadCount() {
+            return bossGroupThreadCount;
+        }
+
+        public void setBossGroupThreadCount(int bossGroupThreadCount) {
+            this.bossGroupThreadCount = bossGroupThreadCount;
+        }
+
+        public int getWorkerGroupThreadCount() {
+            return workerGroupThreadCount;
+        }
+
+        public void setWorkerGroupThreadCount(int workerGroupThreadCount) {
+            this.workerGroupThreadCount = workerGroupThreadCount;
+        }
+
+        public String getMaxPayloadSize() {
+            return maxPayloadSize;
+        }
+
+        public void setMaxPayloadSize(String maxPayloadSize) {
+            this.maxPayloadSize = maxPayloadSize;
+        }
+
+        public String getLeakDetectorLevel() {
+            return leakDetectorLevel;
+        }
+
+        public void setLeakDetectorLevel(String leakDetectorLevel) {
+            this.leakDetectorLevel = leakDetectorLevel;
+        }
+    }
+
+}
